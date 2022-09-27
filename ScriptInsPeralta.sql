fc069debf42bee71c269cf4683c4b71fae440028
Zibete <matiasabelperalta@gmail.com>
2022-09-27 09:37:33 -0300

    Script de inserción de datos

---
+++ ScriptInsPeralta.sql
@@ -0,0 +1,33 @@
+INSERT INTO lst_estados 
+VALUES 	(1, "Activo"),
+		(2, "Inactivo"),
+		(3, "Trasladado"),
+		(4, "Desvinculado");
+
+INSERT INTO lst_generos 
+VALUES 	(1, "Hombre"),
+		(2, "Mujer");
+		
+INSERT INTO lst_novedades
+VALUES 	(1, "Llegada tarde"),
+		(2, "Licencia con justificativo"),
+		(3, "Ausencia sin justificativo"),
+		(4, "Categoría superior"),
+		(5, "Franco trabajado"),
+		(6, "Día de estudio");
+        
+INSERT INTO lst_puestos
+VALUES 	(1, "Cajero/a"),
+		(2, "Repositor/a"),
+		(3, "Vendedor/a de Playa"),
+		(4, "Coordinador/a Adiminstrativo/a"),
+        (5, "Supervisor/a"),
+		(6, "Administrador/a");
+        
+INSERT INTO lst_turnos
+VALUES 	(1, "N", "Turno noche: 22 a 06 hs."),
+		(2, "M", "Turno mañana: 06 a 14 hs."),
+		(3, "T", "Turno tarde: 14 a 22 hs."),
+		(4, "I", "Turno intermedio: 10 a 18 hs."),
+        (5, "F", "Franco"),
+		(6, "V", "Vacaciones");
